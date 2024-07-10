b5f9ec1b29b2aae3cef554da1c227f92ff8a86d7
Lilah Rosenfield <lilah.rosenfield@rideuta.com>
2024-07-10 11:12:14 -0600

    QOL improvements & refactor
    Refactor app classitems into their own module. Adds tools for pickling GTFS feeds that have been correctly loaded, as well as a first cut at exporting edges to human-readable JSON.

--- ridership_view.py
+++ ridership_view.py
@@ -17,148 +17,26 @@ if runtime.exists():
 import ridership_functions as rfx
 import ridership_plots as rplt
 import pickle
+import json
+from ridershipViewObjects import InteractableModelItem as Item
+from ridershipViewObjects import AppClass
 # import folium
 from nicegui import run, ui, context, app, events
 import pandas
-import gtfs_functions
+#import gtfs_functions
 import datetime as dt
 
 session = None # I have no memory of what this does
 
 # Classes
-class AppClass:
-    def __init__(self):
-        self.is_feed_loading = False
-        self.mapLayers = {}
-        self.edgesDict = {}
-        self.mapItem = None
-        self.feedURI = None
-        self._csvURI = None
-        self._isFeedLoaded = False
-        self._feed = None
-        # self.get_route_dirs()
 
-    @property
-    def feed(self):
-        return self._feed
-    
-    @feed.setter
-    def feed(self, other):
-        assert isinstance(other, gtfs_functions.Feed)
-        self._feed = other
-        self.is_feed_loading = False
-        self._isFeedLoaded = True
-
-    @property
-    def isFeedLoaded(self):
-        return self._isFeedLoaded
-
-    @property
-    def isAppReady(self):
-        ready = self.isFeedLoaded and (self.csvURI is not None)
-        return ready
-
-    def load_feed(self):
-        # TODO: refactor to enable weekends
-        sid = gtfs_functions.Feed(self.feedURI).busiest_service_id
-        self._feed = gtfs_functions.Feed(
-                                            self.feedURI, 
-                                            service_ids=[sid],
-                                            time_windows=[0,24]
-                                        )
-        logging.info("Loading GTFS Feed")
-        self.is_feed_loading = True
-        self.feed.avg_speeds
-        logging.info("GTFS Feed Loaded")
-        return self.feed
-
-    def clearMapLayers(self):
-        self.mapLayers = {}
-        activate_map_function.refresh()
-        self.dialogCleanup()
-
-    def dialogCleanup(self):
-        self.dialog.close()
-        self.dialog.clear()
-
-    def openDialog(self):
-        logging.info('Confirming user wants to clear')
-        with ui.dialog() as self.dialog:
-            with ui.card():
-                ui.label('Do you really want to clear all lines from the map?')
-                with ui.row().classes('self-center'):
-                    ui.button('Yes', on_click=self.clearMapLayers)
-                    ui.button('No', on_click=self.dialogCleanup)
-        self.dialog.open()
-
-        
-
-class Item:
-    def __init__(self):
-        self.value = None
-        self._valuesList = [] # Thank g-d for non-static typing
-        self._visibility = False
-        self.columns = []
-        self._valuesMap = None
-        self._valuesMapIsSet = False
-
-    @property
-    def valuesList(self):
-        if self._valuesMapIsSet:
-            return list(self._valuesMap)
-        else: 
-            return self._valuesList
-    
-    def setValuesDF(self, values):
-        """
-        Gets a list of dicts, converts to a PD dataframe, and sets as values
-        """
-        assert isinstance(values, list)
-        self.valuesList = pandas.DataFrame(values)
-        
-    def setValue(self,other):
-        # logging.info(f"Updating value of object to {other}")
-        self.value = other
-
-    @valuesList.setter
-    def valuesList(self, values):
-        """
-        Toggles visibility on for an item and loads the values list
-        """
-        if self._valuesMapIsSet:
-            raise AttributeError(
-                "Cannot set valuesList directly when valuesMap is set"
-                )
-        else:
-            self.visibility = True
-            self._valuesList = values
-
-    
-    @property
-    def visibility(self):
-        return self._visibility
-    
-    @visibility.setter
-    def visibility(self, other):
-        assert isinstance(other, bool)
-        self._visibility = other
-
-    @property
-    def valuesMap(self):
-        return self._valuesMap
-    
-    @valuesMap.setter
-    def valuesMap(self, other):
-        assert isinstance(other, dict)
-        self._valuesMap = other
-        self._valuesMapIsSet = True
 
 # Set up objects
 appObject = AppClass()
 routes = Item()
 dirs = Item()
 prodToggle = Item()
-stops = Item()
+stopsTable = Item()
 buttonObject = Item()
 
 buttonObject.visibility = False
@@ -166,7 +44,7 @@ prodToggle.valuesMap = {
     'Productivity': 'productivity_activity',
     'Speed': 'speed'
     }
-prodToggle.value = 'Productivity'
+prodToggle.userValue = 'Productivity'
 
 # Refreshable UI Elements
 @ui.refreshable 
@@ -175,7 +53,7 @@ def routes_dropdown_function():
                             with_input=True,
                             on_change=update_dirs)
     routesel.bind_visibility_from(routes, 'visibility')
-    routesel.bind_value(routes, 'value')
+    routesel.bind_value(routes, 'userValue')
     routesel.classes('w-full')
 
 @ui.refreshable
@@ -185,33 +63,39 @@ def list_dirs_function():
         dirs_label.classes('self-center')
         dirs_label.bind_visibility_from(dirs, 'visibility')
         dirs_toggle = ui.toggle(dirs.valuesList, on_change=update_stops)
-        dirs_toggle.bind_value(dirs, 'value')
+        dirs_toggle.bind_value(dirs, 'userValue')
         dirs_toggle.bind_visibility_from(dirs, 'visibility')
         
 
 @ui.refreshable
 def list_stops_function():
     stops_table = ui.table(
-        columns=stops.columns, rows = stops.valuesList, selection='multiple')
+            columns=stopsTable.columns, 
+            rows = stopsTable.valuesList,
+            selection='multiple'
+            )
     stops_table.classes('w-full flex-grow')
     stops_table.props('virtual-scroll')
     stops_table.style("max-height: 70vh")
     # Take note, button visibility is bound to stops visibility
-    stops_table.bind_visibility_from(stops, 'visibility')
+    stops_table.bind_visibility_from(stopsTable, 'visibility')
+    route_dir = (routes.userValue, dirs.userValue)
+    if route_dir in appObject.edgesDict:
+        stops_table.selected = appObject.edgesDict[route_dir]
 
-    # And we store the selected values in the button
-    stops_table.on_select(lambda: buttonObject.setValuesDF(stops_table.selected))
+    # And we store the selected values
+    stops_table.on_select(lambda: stoplist_sel(stops_table.selected))
 
 @ui.refreshable
 def activate_button_function():
     activate_button = ui.button(on_click=update_map)
     activate_button.classes('self-center')
-    activate_button.bind_visibility_from(stops, 'visibility')
+    activate_button.bind_visibility_from(stopsTable, 'visibility')
 
     # The button object's visibility property is used to determine
     # if the button is activated
     activate_button.bind_enabled_from(buttonObject, 'visibility')
-    activate_button.bind_text_from(buttonObject, 'value')
+    activate_button.bind_text_from(buttonObject, 'userValue')
 
 @ui.refreshable
 def activate_map_function():
@@ -234,18 +118,37 @@ def set_routes():
     routes.valuesList = route_list
 
 async def start_feedload():
-    appObject.is_feed_loading = True
+    """
+    Asynchronously loads a feed (defined by appObject.feedURI) from either
+        a Zipped GTFS or a pickle
+    """
+    appObject.isFeedLoading = True
     routes.visibility = False
-    appObject.feed = await run.cpu_bound(appObject.load_feed)
-    routes.visibility = True
+    logging.info("Beginning to load feed")
+    if os.path.splitext(appObject.feedURI)[1] == '.pkl':
+        logging.info('Detected pickled feed, loading!')
+        with open(appObject.feedURI, 'rb') as thePickle:
+            brineless = pickle.load(thePickle)
+            appObject.feed = brineless
+        routes.visibility = True
+    elif os.path.splitext(appObject.feedURI)[1] == '.zip':
+        logging.info('Detected zipped feed, loading!')
+        appObject.feed = await run.cpu_bound(appObject.load_feed)
+        appObject.needsPickling = True
+        routes.visibility = True   
+    
+    else:
+        appObject.isFeedLoading = False
+        raise ValueError(f'File {appObject.feedURI} has wrong extension')
 
 def update_dirs():
-    logging.info(f"Updating direction for route number {routes.value}")
+    logging.info(f"Updating direction for route number {routes.userValue}")
     
     # Note: adds previously undefined object rid to Item 'routes'
-    if routes.value is not None:
-        rid = rfx.route_id_from_route_num(appObject.feed, routes.value)
-        routes.speed_segs = rfx.pd_checkfilter(appObject.feed.avg_speeds,'route_id', rid)
+    if routes.userValue is not None:
+        rid = rfx.route_id_from_route_num(appObject.feed, routes.userValue)
+        routes.speed_segs = rfx.pd_checkfilter(
+            appObject.feed.avg_speeds,'route_id', rid)
         trips = rfx.pd_checkfilter(appObject.feed.trips, 'route_id', rid)
         dirs_list = list(set(trips['direction_id']))
         dirs.valuesList = dirs_list
@@ -253,47 +156,76 @@ def update_dirs():
         logging.info("No route provided, holding")
     list_dirs_function.refresh()
 
-def update_stops():
-    # print(f"Updating stops for route {routes.value} in direction {dirs.value}")
-    route_in_dir = rfx.pd_checkfilter(
-        routes.speed_segs, 'direction_id', dirs.value)
-    stop_only = route_in_dir[['stop_sequence', 'start_stop_name']]
-    stops.valuesList = stop_only.rename(
-        columns={'stop_sequence':'id'}).to_dict('records')
+def stoplist_sel(ls):
+    stopsTable.userValue = ls
     buttonObject.visibility = True
-     # Note: adds previously undefined object columns to Item 'stops'
-    stops.columns = [
+
+def update_stops():
+    """
+    Based on the route & direction identified by respective model objects,
+        loads a table of stops into the stopsTable object
+        refreshes the list view and the map update button
+    """
+    # Filters the GTFS speed segments
+    route_in_dir = rfx.pd_checkfilter(
+        routes.speed_segs, 'direction_id', dirs.userValue)
+    stop_only = route_in_dir[['stop_sequence', 'start_stop_name']]
+    
+    # Prettifies and creates the columns
+    stopsTable.valuesList = stop_only.rename(
+        columns={'stop_sequence':'id'}).to_dict('records')
+    buttonObject.visibility = False
+    
+    # This is currently the only use of Item.columns()
+    stopsTable.columns = [
         {'name':'start_stop', 'label':'Stop', 'field':'start_stop_name'}
         ]
-    routes_dirs = (routes.value, dirs.value)
-    # print(f"Checking if {routes_dirs} is in {appObject.mapLayers}")
+    routes_dirs = (routes.userValue, dirs.userValue)
+
+    # Adjusts the text of the button if the user has already selected segments
     if routes_dirs in appObject.mapLayers:
         # print("It is!")
-        buttonObject.value = "Update Segments"
+        buttonObject.userValue = "Update Segments"
     else:
-        buttonObject.value = "Calculate Productivity"
+        buttonObject.userValue = "Calculate Productivity"
+    
+    # Refreshes
     list_stops_function.refresh()
     activate_button_function.refresh()
 
 def update_map():
+    """
+    Updates the map with a given route, direction, & edges found in the 
+    relevant Item() model objects 
+    """
     buttonObject.visibility = False
-    ui.notify(f'Calculating {prodToggle.value}')
-    edges = set(buttonObject.valuesList['id'].tolist())
+    ui.notify(f'Calculating {prodToggle.userValue}')
+
+    # Setting up the edges from the checked values
+    edges = set(pandas.DataFrame(stopsTable.userValue)['id'].tolist())
     edges = {v - 1 for v in edges}
     edges.add(0)
     edges_lst = list(edges)
     edges_lst.sort()
+    
+    # Create the layer for the routes
     newlyr = rplt.build_lyr_for_route(
-        appObject.feed, routes.value, edges_lst, dirs.value,
-        highlight=prodToggle.valuesMap[prodToggle.value],
+        appObject.feed, routes.userValue, edges_lst, dirs.userValue,
+        highlight=prodToggle.valuesMap[prodToggle.userValue],
         csv=appObject.csvURI)
-    route_dir = (routes.value, dirs.value)
+    route_dir = (routes.userValue, dirs.userValue)
+    
+    # Places the layer and the edges into dictionaries
     appObject.mapLayers[route_dir] = newlyr
-    appObject.edgesDict[route_dir] = edges
-    buttonObject.value = "Update Segments"
+    appObject.edgesDict[route_dir] = stopsTable.userValue
+    buttonObject.userValue = "Update Segments"
     activate_map_function.refresh()
 
 def download_map():
+    """
+    Saves a raw Leaflet map to the folder Saved Maps with a title defined by
+        the time marker
+    """
     html_doc= appObject.mapItem.get_root()._repr_html_()
     dirmk('Saved Maps')
     time_marker = str(
@@ -304,11 +236,83 @@ def download_map():
         o.write(html_doc)
     #ui.download(bytes(html_doc, encoding='utf-8'), 'productivity.html')
 
+def save_seg_def():
+    """
+    Saves a JSON containing the segment information to the folder Exports
+        with a title defined by the time marker
+    """
+    dirmk('Saved Segments')
+    time_marker = str(
+        dt.datetime.today().replace(microsecond=0)
+        ).replace(":","")
+    
+    # IGNOREME
+    # edges_cleaned = {}
+    # for e in appObject.edgesDict:
+    #     for 
+    #     e_str = f'Route {e[0]} Direction {e[1]}'
+    #     logging.info(f'cleaning {e_str}')
+    #     edges_cleaned[e_str] = pandas.DataFrame(appObject.edgesDict[e])
+    # logging.info(f'edges cleaned looks like {edges_cleaned}')
+    # e_df = pandas.concat(edges_cleaned)
+    # logging.info(f'e_df looks like{e_df} and is of type {type(e_df)}')
+    
+    dump = rearrange_and_convert_to_json(appObject.edgesDict)
+    with open(f'Saved Segments\\Export {time_marker}.json', 'w') as o:
+        logging.info("Exporting segments info")
+        o.write(dump)
+
+def rearrange_and_convert_to_json(data):
+    """
+    Helper function, created with the support of a large pile of linear algebra
+    """
+    rearranged_data = {}
+
+    for (rt, dir), records in data.items():
+        if rt not in rearranged_data:
+            rearranged_data[rt] = {}
+        rearranged_data[rt][dir] = records
+
+    # Convert the rearranged dictionary to a JSON string
+    json_data = json.dumps(rearranged_data, indent=4)
+
+    return json_data
+
+
 # def add_number() -> None:
 #    numbers.appObjectend(random.randint(0, 100))
 #    number_ui.refresh()
 
 # Build the webpage
+
+def open_dialog():
+        """
+        Opens a dialog confirming user wants to clear the map
+
+        Encapsulated in appObject because we need to control the dialog box
+        from helper functions
+        """
+        logging.info('Confirming user wants to clear')
+        with ui.dialog() as appObject.dialog:
+            with ui.card():
+                ui.label('Do you really want to clear all lines from the map?')
+                with ui.row().classes('self-center'):
+                    ui.button('Yes', on_click=clear_clean)
+                    ui.button('No', on_click=dialog_cleanup)
+        appObject.dialog.open()
+
+
+    #activate_map_function.refresh()
+
+def clear_clean():
+    appObject.clearMapLayers()
+    dialog_cleanup()
+
+
+def dialog_cleanup():
+    appObject.dialog.close()
+    appObject.dialog.clear()
+    activate_map_function.refresh()
 
 @ui.page('/application')
 def application():
@@ -324,13 +328,19 @@ def application():
                 ui.label('Route Productivity Monitor').classes('self-center')
                 with ui.row().classes('w-full justify-right'):
                     with ui.dropdown_button('Options'):
+                            ui.item('Save Segments', on_click=save_seg_def)
                             ui.item('Save Map', on_click=download_map)
-                            ui.item('Clear Map', on_click=appObject.openDialog)
-                    with ui.dropdown_button(prodToggle.valuesList) as prod_toggle:
+                            ui.item('Clear Map', on_click=open_dialog)
+                    with ui.dropdown_button(
+                           prodToggle.valuesList
+                        ) as prod_toggle:
                         #prod_toggle.bind_visibility(dirs, 'visibility')
-                        prod_toggle.bind_text(prodToggle, 'value')
+                        prod_toggle.bind_text(prodToggle, 'userValue')
                         for i in prodToggle.valuesList:
-                            ui.item(i, on_click=lambda i=i: prodToggle.setValue(i))
+                            ui.item(
+                                i, 
+                                on_click=lambda i=i: prodToggle.setUserValue(i)
+                                )
                 routes_dropdown_function()
                 list_dirs_function()
                 list_stops_function()
@@ -366,8 +376,11 @@ with ui.row().classes('self-center'):
 with ui.row().classes('self-center'):
     feed_button = ui.button('Load Feed', on_click=start_feedload)
     feed_button.classes('self-center')
+    pickle_button = ui.button('Pickle Feed', on_click=appObject.pickleFeed)
+    feed_button.classes('self-center')
+    pickle_button.bind_visibility_from(appObject, 'needsPickling')
     spinner = ui.spinner().classes('self-center')
-    spinner.bind_visibility_from(appObject, 'is_feed_loading')
+    spinner.bind_visibility_from(appObject, 'isFeedLoading')
 
 csv_uris = []
 lscsv = os.scandir('CSVs')
